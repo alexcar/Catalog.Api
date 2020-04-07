869774bd48089f69a7ab35d832b46422d08406c6
alexandre.carvalho@live.com <alexandre.carvalho@live.com>
2020-04-07 15:22:18 -0300

    Added health checking

---
+++ src/Catalog.Api/HealthChecks/RedisCacheHealthCheck.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.HealthChecks
+{
+    public class RedisCacheHealthCheck { }
+    
+    //public class RedisCacheHealthCheck : IHealthCheck
+    //{
+    //    private readonly CacheSettings _settings;
+
+    //    public RedisCacheHealthCheck(IOptions<CacheSettings> settings)
+    //    {
+    //        _settings = settings.Value;
+    //    }
+
+    //    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+    //        CancellationToken cancellationToken = default)
+    //    {
+    //        try
+    //        {
+    //            var redis = ConnectionMultiplexer.Connect(_settings.ConnectionString);
+    //            var db = redis.GetDatabase();
+
+    //            var result = await db.PingAsync();
+    //            if (result < TimeSpan.FromSeconds(5))
+    //            {
+    //                return await Task.FromResult(
+    //                    HealthCheckResult.Healthy());
+    //            }
+
+    //            return await Task.FromResult(
+    //                HealthCheckResult.Unhealthy());
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            return await Task.FromResult(
+    //                HealthCheckResult.Unhealthy(e.Message));
+    //        }
+    //    }
+
+    //    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
--- src/Catalog.Api/Startup.cs
+++ src/Catalog.Api/Startup.cs
@@ -28,7 +28,7 @@ namespace Catalog.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
+            services                
                 .AddCatalogContext(Configuration.GetSection("DataSource:ConnectionString").Value)
                 .AddResponseCaching()
                 .AddMemoryCache()
@@ -44,6 +44,11 @@ namespace Catalog.Api
                 //})
                 .AddValidation()
                 .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
+
+            services
+                .AddHealthChecks()
+                .AddSqlServer(Configuration.GetSection("DataSource:ConnectionString").Value);
+
 
             services.AddLinks(config =>
             {
@@ -79,6 +84,7 @@ namespace Catalog.Api
             app.UseHttpsRedirection();            
             app.UseAuthorization();
             app.UseResponseCaching();
+            app.UseHealthChecks("/health");
             app.UseMiddleware<ResponseTimeMiddlewareAsync>();
             app.UseEndpoints(endpoints =>
             {
